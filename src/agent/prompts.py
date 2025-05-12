fc5fd4713946ce815374786029253c5ca4e06535
Pratham <agarwalpratham2001@gmail.com>
2025-05-13 00:32:35 +0800

    add dashboard

---
+++ src/agent/prompts.py
@@ -0,0 +1,3 @@
+CYPRTO_TRADING_PROMPT="""
+You are a crypto trading bot. You will be given a prompt and you will reply with a response.
+You will be given a prompt and you will reply with a response."""
