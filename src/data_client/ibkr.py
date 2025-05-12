bb6f9a06a92ad084564c0f7eeefc509aaeca8210
Pratham <agarwalpratham2001@gmail.com>
2025-05-13 00:31:10 +0800

    ibkr

---
+++ src/data_client/ibkr.py
@@ -0,0 +1,71 @@
+from ibapi.client import EClient
+from ibapi.wrapper import EWrapper
+from ibapi.contract import Contract
+from ibapi.common import BarData
+import threading
+import time
+
+class IBHistoricalDataApp(EWrapper, EClient):
+    def __init__(self):
+        EWrapper.__init__(self)
+        EClient.__init__(self, self)
+
+        self.historical_data = []
+        self.data_end = False
+
+    def historicalData(self, reqId, bar: BarData):
+        print(f"HistoricalData: Date: {bar.date}, Open: {bar.open}, High: {bar.high}, Low: {bar.low}, Close: {bar.close}, Volume: {bar.volume}")
+        self.historical_data.append(bar)
+
+    def historicalDataEnd(self, reqId: int, start: str, end: str):
+        print(f"HistoricalDataEnd. ReqId: {reqId}, from {start} to {end}")
+        self.data_end = True
+
+    def error(self, reqId, errorCode, errorString, advancedOrderRejectJson=""):
+        print(f"Error: {reqId}, {errorCode}, {errorString}")
+        if advancedOrderRejectJson:
+            print(f"Advanced Order Reject JSON: {advancedOrderRejectJson}")
+
+
+    def start(self):
+        # Define your contract (example: AAPL stock on SMART)
+        contract = Contract()
+        contract.symbol = "AAPL"
+        contract.secType = "STK"
+        contract.exchange = "SMART"
+        contract.currency = "USD"
+
+        # Request historical data
+        queryTime = (time.strftime("%Y%m%d-%H:%M:%S"))
+        self.reqHistoricalData(reqId=1, 
+                               contract=contract,
+                               endDateTime=queryTime,
+                               durationStr="2 D",
+                               barSizeSetting="5 mins",
+                               whatToShow="TRADES",
+                               useRTH=1,
+                               formatDate=1,
+                               keepUpToDate=False,
+                               chartOptions=[])
+
+    def run_app(self):
+        # Start connection thread
+        thread = threading.Thread(target=self.run)
+        thread.start()
+
+        # Give IB Gateway time to connect
+        time.sleep(1)
+
+        self.start()
+
+        # Wait for data to complete
+        while not self.data_end:
+            time.sleep(1)
+
+        self.disconnect()
+
+
+if __name__ == "__main__":
+    app = IBHistoricalDataApp()
+    app.connect("127.0.0.1", 7497, clientId=1)
+    app.run_app()
---
+++ src/feature/trend.py
@@ -0,0 +1,9 @@
+# Trend Features 
+# 1. Market regime: bull, bear, or sideways
+# 2. Spline regression Wahba spline
+# 3. Polynomial regression
+# 4. Exponential smoothing
+# 5. Moving average
+# 6. Kalman filter trend (https://www.quantitativo.com/p/fast-trend-following)
+# 7. Super-trend indicator (https://www.quantifiedstrategies.com/supertrend-indicator-trading-strategy/)
+# ML based trend models
